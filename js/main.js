995c4dddd5951a38e099c8a3b4d42edd65010906
ExodusDG <sanistray@gmail.com>
2022-10-19 10:12:39 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ $('.hamburger').click(function() {
     $('.mobileMenu').toggleClass('menuActive')
     $('body').toggleClass('scroll')
 })
+
+var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $('.mobileMenu').removeClass('menuActive')
+    $('body').removeClass('scroll')
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 600);
+        return false;
+    }); //плавный скролл
