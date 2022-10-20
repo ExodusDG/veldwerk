6a6f4a0b924128795d7d170887123e2d8820d06a
ExodusDG <sanistray@gmail.com>
2022-10-20 10:32:28 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-$('.hamburger').click(function() {
+$('.hamburger').click(function () {
     $(this).toggleClass('is-active')
     $('.mobileMenu').toggleClass('menuActive')
     $('body').toggleClass('scroll')
 })
 
 var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $('.mobileMenu').removeClass('menuActive')
+$('a[href*="#"]').click(function () {
+    $('.mobileMenu').removeClass('menuActive')
+    $('.hamburger').removeClass('is-active')
     $('body').removeClass('scroll')
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 600);
-        return false;
-    }); //плавный скролл
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 600);
+    return false;
+}); //плавный скролл
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.navbar').addClass('navbarFixed')
+    } else {
+        $('.navbar').removeClass('navbarFixed')
+    }
+});
